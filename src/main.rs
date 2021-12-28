a0fd1cb65fe6e87bd54f9e170d929c8f57f4f732
Mikael Rozee <mikaelsrozee@gmail.com>
2021-12-28 17:03:54 +0000

    feat: calculate and display time to complete operation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use clap::Parser;
 
 use crate::cli::Cli;
@@ -7,5 +9,10 @@ mod cli;
 fn main() {
     let cli = Cli::parse();
 
+    let start_time = Instant::now();
+
     println!("Using input {:?}", cli.files);
+
+    let elapsed_time = start_time.elapsed().as_secs_f32();
+    println!("\n✨ Done in {:.2}s.", elapsed_time);
 }
