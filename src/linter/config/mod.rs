0a69911fc9fce0c14b164cb30863c9b67f80e089
Mikael Rozee <mikaelsrozee@gmail.com>
2022-03-01 12:45:52 +0000

    feat(config): allow rules to be disabled

--- src/linter/config/mod.rs
+++ src/linter/config/mod.rs
@@ -8,9 +8,12 @@ use std::{collections::HashMap, fmt::Debug, fs::read_to_string, path::PathBuf};
 pub struct LintConfig {
     /// Values from the "extends" property of configuration
     pub rules: Vec<String>,
+    pub disabled_rules: Vec<String>,
     // Config for quotes rule
     pub quotes: RuleConfig<QuotesConfig>,
 }
+
+// TODO impl default manually
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(untagged)]
@@ -19,7 +22,7 @@ enum Rules {
     /// * 0 - turns the rule off
     /// * 1 - turn the rule on as a warning (doesn't affect exit code)
     /// * 2 - turn the rule on as an error (exit code is 1 when triggered)
-    IntegerEnabled(u8),
+    NumberEnabled(u8),
     /// ESLint rule
     /// * "off" - turns the rule off
     /// * "warn" - turn the rule on as a warning (doesn't affect exit code)
@@ -61,6 +64,22 @@ struct RawConfigFile {
     rules: HashMap<String, Rules>,
 }
 
+fn is_num_enabled(key: String, num: &u8) -> Option<String> {
+    if *num == 0 {
+        Some(key)
+    } else {
+        None
+    }
+}
+
+fn is_string_enabled(key: String, str: &str) -> Option<String> {
+    if str == "off" {
+        Some(key)
+    } else {
+        None
+    }
+}
+
 impl From<PathBuf> for LintConfig {
     fn from(buf: PathBuf) -> Self {
         // If buf is default, the user did not specify a config file to load
@@ -86,16 +105,38 @@ impl From<PathBuf> for LintConfig {
         if ext == "js" {
             todo!()
         } else if ext == "json" {
+            // Parse file from JSON to struct
             let file = read_to_string(buf).unwrap();
             let json: RawConfigFile = serde_json::from_str(file.as_str()).unwrap();
 
+            // Get list of disabled rules
+            let disabled_rules: Vec<String> = json
+                .rules
+                .iter()
+                .map(|(key, value)| match value {
+                    Rules::NumberEnabled(num) => is_num_enabled(key.to_string(), num),
+                    Rules::NumberEnabledString(num, _) => is_num_enabled(key.to_string(), num),
+                    Rules::NumberEnabledObject(num, _) => is_num_enabled(key.to_string(), num),
+                    Rules::StringEnabledObject(str, _) => is_string_enabled(key.to_string(), str),
+                    Rules::StringEnabled(str) => is_string_enabled(key.to_string(), str),
+                    Rules::StringEnabledString(str, _) => is_string_enabled(key.to_string(), str),
+                })
+                .flatten()
+                .collect();
+
+            // If the eslint:all property is extended from, use all rules apart from those in disabled_rules
             if json.extends.contains(&all_rules) {
                 LintConfig {
                     rules: [all_rules].to_vec(),
+                    disabled_rules,
                     ..Default::default()
                 }
             } else {
-                LintConfig::default()
+                // Otherwise, we return the list of disabled rules and the default config
+                LintConfig {
+                    disabled_rules,
+                    ..Default::default()
+                }
             }
         } else {
             // TODO: parse other file types
